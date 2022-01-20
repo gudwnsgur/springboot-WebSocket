ef0c2576e6f6d6f5c2bac6df6a712f5d23ff55bb
형준혁 <gudwnsgur12@naver.com>
2022-01-20 17:37:55 +0900

    first commit

---
+++ src/main/kotlin/com/example/springbootwebSocket/SpringbootWebSocketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootwebSocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootWebSocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootWebSocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootwebSocket/SpringbootWebSocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootwebSocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootWebSocketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
